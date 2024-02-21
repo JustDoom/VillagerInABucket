35d7f68845db4db521c9ac9d3516d69ad8f1fc3e
JustDoom <61824552+JustDoom@users.noreply.github.com>
2024-02-21 17:04:03 +1000

    Add all Villager Types to creative tab

--- common/src/main/java/com/imjustdoom/villagerinabucket/VillagerInABucket.java
+++ common/src/main/java/com/imjustdoom/villagerinabucket/VillagerInABucket.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.DataResult;
 import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
@@ -16,13 +17,22 @@ import net.minecraft.world.entity.npc.VillagerType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VillagerInABucket {
     public static final String MOD_ID = "villagerinabucket";
 
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(MOD_ID, Registries.CREATIVE_MODE_TAB);
+
+    private static final Map<VillagerType, Integer> VILLAGER_DATA_LIST = new HashMap<>() {
+        {
+            put(VillagerType.DESERT, 1);
+            put(VillagerType.SAVANNA, 2);
+            put(VillagerType.SNOW, 3);
+            put(VillagerType.SWAMP, 4);
+        }
+    };
 
     public static final RegistrySupplier<CreativeModeTab> VILLAGERINABUCKET_TAB = TABS.register(
             "villagerinabucket_tab",
@@ -30,20 +40,16 @@ public class VillagerInABucket {
                 builder.title(Component.translatable("category.villagerinabucket.villagerinabucket_tab"));
                 builder.icon(() -> new ItemStack(ModItems.VILLAGER_IN_A_BUCKET.get()));
                 builder.displayItems((params, output) -> {
-                    List<VillagerData> villagerDataList = Arrays.asList(
-                            new VillagerData(VillagerType.DESERT, VillagerProfession.NONE, 0),
-                            new VillagerData(VillagerType.SAVANNA, VillagerProfession.NONE, 0),
-                            new VillagerData(VillagerType.SNOW, VillagerProfession.NONE, 0),
-                            new VillagerData(VillagerType.SWAMP, VillagerProfession.NONE, 0)
-                    );
-
-                    // TODO: Maybe add every type
-                    for (int i = 1; i <= 4; i++) {
+                    // TODO: Try account for modded villager types and add them to the creative menu (with our default texture)
+                    for (VillagerType type : BuiltInRegistries.VILLAGER_TYPE) {
                         ItemStack itemStack = new ItemStack(ModItems.VILLAGER_IN_A_BUCKET.get());
                         CompoundTag compoundTag = itemStack.getOrCreateTag();
-                        compoundTag.putInt("CustomModelData", i);
 
-                        VillagerData villagerData = villagerDataList.get(i - 1);
+                        if (VILLAGER_DATA_LIST.containsKey(type)) {
+                            compoundTag.putInt("CustomModelData", VILLAGER_DATA_LIST.get(type));
+                        }
+
+                        VillagerData villagerData = new VillagerData(type, VillagerProfession.NONE, 0);
                         DataResult<Tag> data = VillagerData.CODEC.encodeStart(NbtOps.INSTANCE, villagerData);
 
                         data.resultOrPartial(s -> System.out.println("Oh no something happened, no idea how or what the consequences are. Contact Villager In A Bucket support though")).ifPresent(tag -> compoundTag.put("VillagerData", tag));
