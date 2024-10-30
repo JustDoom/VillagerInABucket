b3c481b5a46deee4c139788798c8fc77c7f31faa
doom <61824552+JustDoom@users.noreply.github.com>
2024-10-30 15:35:37 +1000

    Fix Villagers not being picked up by dispensers

--- common/src/main/java/com/imjustdoom/villagerinabucket/mixin/DispenseBucketBehaviorMixin.java
+++ common/src/main/java/com/imjustdoom/villagerinabucket/mixin/DispenseBucketBehaviorMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-@Mixin(targets = "net/minecraft/core/dispenser/DispenseItemBehavior$17")
+@Mixin(targets = "net/minecraft/core/dispenser/DispenseItemBehavior$7")
 public abstract class DispenseBucketBehaviorMixin {
 
     @Inject(method = "execute", at = @At("HEAD"), cancellable = true)
