658f6ad7eab6098b702d344aeee2d4d894e01e2f
doom <61824552+JustDoom@users.noreply.github.com>
2025-01-15 12:57:21 +1000

    Add zombie villager to dispenser

--- common/src/main/java/com/imjustdoom/villagerinabucket/VillagerInABucket.java
+++ common/src/main/java/com/imjustdoom/villagerinabucket/VillagerInABucket.java
@@ -48,6 +48,7 @@ public class VillagerInABucket {
 
         DispenserBlock.registerBehavior(ModItems.VILLAGER_IN_A_BUCKET, dispenseItemBehavior);
         DispenserBlock.registerBehavior(ModItems.WANDERING_TRADER_IN_A_BUCKET, dispenseItemBehavior);
+        DispenserBlock.registerBehavior(ModItems.ZOMBIE_VILLAGER_IN_A_BUCKET, dispenseItemBehavior);
     }
 
     public static void init() {
