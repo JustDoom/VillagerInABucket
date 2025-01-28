931ac34b452fc55ad1cf471b2b76f86b8b0edb21
doom <61824552+JustDoom@users.noreply.github.com>
2025-01-28 20:22:17 +1000

    Update wandering trader sound

--- common/src/main/java/com/imjustdoom/villagerinabucket/mixin/WanderingTraderMixin.java
+++ common/src/main/java/com/imjustdoom/villagerinabucket/mixin/WanderingTraderMixin.java
@@ -132,6 +132,6 @@ public abstract class WanderingTraderMixin extends AbstractVillager implements B
 
     @Override
     public @NotNull SoundEvent getPickupSound() {
-        return SoundEvents.VILLAGER_TRADE;
+        return SoundEvents.WANDERING_TRADER_NO;
     }
 }
