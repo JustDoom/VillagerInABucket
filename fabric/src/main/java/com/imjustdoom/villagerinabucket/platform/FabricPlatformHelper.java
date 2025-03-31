dd63b5d2441f8fbb821b2ad693c3c824bd95c5f6
Preston <61824552+JustDoom@users.noreply.github.com>
2025-03-31 14:41:46 +1000

    Begin on 1.21.5 update

---
+++ fabric/src/main/java/com/imjustdoom/villagerinabucket/platform/FabricPlatformHelper.java
@@ -0,0 +1,22 @@
+package com.imjustdoom.villagerinabucket.platform;
+
+import com.imjustdoom.villagerinabucket.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
---
+++ forge/src/main/java/com/imjustdoom/villagerinabucket/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.imjustdoom.villagerinabucket.platform;
+
+import com.imjustdoom.villagerinabucket.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
